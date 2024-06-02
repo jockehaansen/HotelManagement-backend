2f7c3541cdf3c443f80c7c368e3db77e4705a532
Joakim Hansen <jocke.hansen@outlook.com>
2024-06-02 21:40:48 +0200

    Models and repos started

---
+++ src/main/java/com/example/hotellmanagersystem/services/impl/AddressServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.hotellmanagersystem.services.impl;
+
+import com.example.hotellmanagersystem.services.AddressService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AddressServiceImpl implements AddressService {
+}
