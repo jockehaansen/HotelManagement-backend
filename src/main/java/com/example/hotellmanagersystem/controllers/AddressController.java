68856b91d011ea1365c13ca8ddab7321ef76a6dd
Joakim Hansen <jocke.hansen@outlook.com>
2024-06-05 13:29:00 +0200

    AddressController logic implemented

--- src/main/java/com/example/hotellmanagersystem/controllers/AddressController.java
+++ src/main/java/com/example/hotellmanagersystem/controllers/AddressController.java
@@ -1,28 +1,58 @@
 package com.example.hotellmanagersystem.controllers;
 
+import com.example.hotellmanagersystem.DTO.Basic.BasicAddressDTO;
+import com.example.hotellmanagersystem.DTO.Detailed.DetailedAddressDTO;
+import com.example.hotellmanagersystem.models.Address;
+import com.example.hotellmanagersystem.services.AddressService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/address")
+@RequiredArgsConstructor
 public class AddressController {
 
-    @PostMapping("/create")
-    void createAddress(){
+    private final AddressService addressService;
 
+    @PostMapping("/create")
+    Address createAddress(@RequestBody Address address){
+        return addressService.createAddress(address);
     }
 
     @DeleteMapping("/delete/{id}")
-    void deleteById(@PathVariable Long id){
-
+    String deleteById(@PathVariable Long id){
+        return addressService.deleteAddressById(id);
     }
 
-    @PutMapping("/update/{id}")
-    void updateById(@PathVariable Long id){
-
+    @PutMapping("/update")
+    Address updateAddress(@RequestBody Address address){
+        return addressService.updateAddress(address);
     }
 
     @GetMapping("")
-    void getAllAddresses(){
+    List<Address> getAllAddresses(){
+        return addressService.getAllAddresses();
+    }
 
+    @GetMapping("/basic")
+    List<BasicAddressDTO> getAllAddressesAsBasicDTOs(){
+        return addressService.getAllAddressesAsBasicDTO();
+    }
+
+    @GetMapping("/detailed")
+    List<DetailedAddressDTO> getAllAddressesAsDetailedDTOs(){
+        return addressService.getAllAddressesAsDetailedDTO();
+    }
+
+    @GetMapping("/basic/{id}")
+    BasicAddressDTO getAddressAsBasicDTO(@PathVariable Long id){
+        return addressService.getBasicAddressDTOById(id);
+    }
+
+    @GetMapping("/detailed/{id}")
+    DetailedAddressDTO getAddressAsDetailedDTO(@PathVariable Long id){
+        return addressService.getDetailedAddressDTOById(id);
     }
 }
