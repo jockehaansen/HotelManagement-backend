f661ba9967e6d2a8601606874f3cbf67fc617bae
Joakim Hansen <jocke.hansen@outlook.com>
2024-06-09 16:57:54 +0200

    booking service methods started

--- src/main/java/com/example/hotellmanagersystem/models/Booking.java
+++ src/main/java/com/example/hotellmanagersystem/models/Booking.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.cglib.core.Local;
 
 import java.time.LocalDate;
 import java.util.List;
@@ -17,6 +18,7 @@ public class Booking {
     @Id
     @GeneratedValue
     private Long id;
+    //denna ska auto-genereras i framtiden
     private Long bookingNumber;
     private LocalDate startDate;
     private LocalDate endDate;
@@ -32,4 +34,13 @@ public class Booking {
 
     @OneToMany
     private List<Room> rooms;
+
+    public Booking(LocalDate startDate, LocalDate endDate, double totalPrice, LocalDate created, Customer customer, List<Room> rooms){
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.totalPrice = totalPrice;
+        this.created = created;
+        this.customer = customer;
+        this.rooms = rooms;
+    }
 }
